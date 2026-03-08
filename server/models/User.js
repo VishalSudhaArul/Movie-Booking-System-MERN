eca514c6d494633b92cb494f7dcc2fe0997d14ad
Vishal <vishaarul2005@email.com>
2026-03-09 00:04:54 +0530

    added admin middleware and improved user authentication

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,54 @@
 // User.js
 
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//     name:String,
+//     email:String,
+//     password:String,
+//     role:{
+//         type:String,
+//         default:"user"
+//     }
+// });
+
+// module.exports = mongoose.model("User", userSchema);
+
+
+
+
+
+// models/User.js
+
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String,
+const userSchema = new mongoose.Schema(
+{
+    name:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
     role:{
         type:String,
+        enum:["user","admin","theaterOwner"],
         default:"user"
     }
+
+},
+{
+    timestamps:true
 });
 
 module.exports = mongoose.model("User", userSchema);
